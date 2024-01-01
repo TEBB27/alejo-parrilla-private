61688648af9374e9ea56c97265f86bc37d5fb115
TOMAS ESTEBAN BRICEÑO BERNAL <110701704+TEBB27@users.noreply.github.com>
2023-12-31 21:03:54 -0500

    Primer commit

---
+++ project/app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import menu, contact, booking, recomendation
+# Register your models here.
+admin.site.register(menu)
+admin.site.register(contact)
+admin.site.register(booking)
+admin.site. register(recomendation)
---
+++ project/app/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+CATEGORY = [
+    ('Carnes', 'Carnes'),
+    ('Pescados', 'Pescados'),
+    ('Típicos', 'Típicos'), 
+    ('Bowls', 'Bowls'),
+    ('Menú Infantil', 'Menú Infantil'),
+]
+
+class menu (models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length =1000)
+    category = models.CharField(choices = CATEGORY, max_length = 1000)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu', null=True, blank=True)
+    is_available = models.BooleanField(default=True)
+    def __str__(self):
+        return self.name
+    
+class contact(models.Model):
+    email = models.EmailField()
+    
+class booking (models.Model):
+    name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=12)
+    date = models.DateField()
+    time = models.TimeField()
+    seats = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+class recomendation(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length =1000)
+    image = models.ImageField(upload_to='recomendation', null=True, blank=True)
+    def __str__(self):
+        return self.name
