6a4101ce6ae8e40318c156d85cb6494efd314f89
TOMAS ESTEBAN BRICEÑO BERNAL <110701704+TEBB27@users.noreply.github.com>
2024-01-02 14:13:39 -0500

    Creación de verificación de cooreo para la newsletter

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.response import Response
 from .serializers import menuSerializer, bookingSerializer, contactSerializer, recomendationSerializer
 from .models import menu, contact, booking, recomendation
 # Create your views here.
@@ -11,6 +13,16 @@ class menuViewSet(viewsets.ModelViewSet):
 class contactViewSet(viewsets.ModelViewSet):
     queryset = contact.objects.all()
     serializer_class = contactSerializer
+    
+    def create(self, request):
+        email = request.data['email']
+        try:
+            contact.objects.get(email=email)
+            return Response({'message': 'El correo ya existe en la base de datos'})
+        except ObjectDoesNotExist:
+            client = contact(email=email)
+            client.save()
+            return Response({'message': 'El correo se ha registrado satisfactoriamente'})
     
 class bookingViewSet(viewsets.ModelViewSet):
     queryset = booking.objects.all()
